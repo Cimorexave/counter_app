ea42d6ec84b9460ac5116941db49f80a8a16b59e
Cimorexave <sadeq.s502@gmail.com>
2024-04-12 12:52:21 +0330

    setting up hive connection and paths

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,33 @@
-import 'package:counter_app/pages/home.dart';
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
+import 'package:counter_app/model/profile.dart';
+import 'package:counter_app/model/record.dart';
+import 'package:counter_app/pages/home.dart';
+import 'package:counter_app/pages/profile.dart';
+import 'package:flutter/foundation.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+
+void main() async {
   // init hive
+  if (!kIsWeb) {
+    if (Platform.isAndroid) {
+      Directory appDocumentDirectory = await getApplicationDocumentsDirectory();
+      Hive.init(appDocumentDirectory.path);
+    } else {
+      Hive.initFlutter();
+    }
+  }
 
   // register adapters
+  Hive.registerAdapter(ProfileAdapter());
+  Hive.registerAdapter(RecordAdapter());
 
   // open boxes
-
-  // select profile
+  await Hive.openBox<Profile>('profiles');
+  await Hive.openBox<Record>('records');
 
   runApp(const MainApp());
 }
@@ -18,8 +37,17 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => Home(),
+        '/profile': (context) => ProfilePage(),
+      },
+      initialRoute: Hive.box<Profile>("profiles")
+              .values
+              .any((element) => element.isLastUsed)
+          ? "/profile"
+          : "/",
       home: Home(),
     );
   }
